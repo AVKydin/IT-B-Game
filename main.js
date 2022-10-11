541f2923c7cc05b859c5a80676a046acbfd48fa3
AVKydin <tlalok@ukr.net>
2022-10-11 21:30:22 +0300

    finished the first day of the marathon

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let countClick = 0;
 let blockCount = document.querySelector("#count");
 let btn2 = document.querySelector("#btn2");
 
-console.dir(btn2);
+// console.dir(btn2);
 
 btn2.onclick = function () {
     p2 = document.querySelector("#p2");
@@ -17,13 +17,25 @@ btn2.onclick = function () {
     p2.style.color = "blue";
 }
 
-console.dir(blockCount);
+// console.dir(blockCount);
 
 function btnClick() {
     countClick += 1;
     blockCount.innerText = countClick;
 }
 
+/*
+1. Вибрати аудіо тег
+2. Вибрати кнопку
+3. Зробити клік по кнопці
+4. При кліці включити музику
+*/
 
+let audioGun = document.querySelector("#audioGun");
+// console.dir(audioGun);
+let btn3 = document.querySelector("#btn3");
 
+btn3.onclick = function () {
+    audioGun.play();
+}
     
